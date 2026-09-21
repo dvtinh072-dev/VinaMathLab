17b10f2321b219b079026b27ab7cd3f4cdb8f25f
dvtinh072-dev <dvtinh072@gmail.com>
2026-09-21 20:46:09 +0700

    fix(grade6): loai bo triet de cac bai cu chuong II bi de len du lieu moi va cap nhat day du index.ts

--- src/data/grade6/chapter2/index.ts
+++ src/data/grade6/chapter2/index.ts
@@ -4,8 +4,18 @@ import { LESSON_9_DATA } from './lesson9';
 import { LESSON_10_DATA } from './lesson10';
 import { LESSON_11_DATA } from './lesson11';
 import { LESSON_12_DATA } from './lesson12';
+import { LESSON_13_DATA } from './lesson13';
+import { GRADE_6_CHAPTER_2_REVIEW_LESSON } from './chapter2Review';
 
-export { LESSON_8_DATA, LESSON_9_DATA, LESSON_10_DATA, LESSON_11_DATA, LESSON_12_DATA };
+export {
+  LESSON_8_DATA,
+  LESSON_9_DATA,
+  LESSON_10_DATA,
+  LESSON_11_DATA,
+  LESSON_12_DATA,
+  LESSON_13_DATA,
+  GRADE_6_CHAPTER_2_REVIEW_LESSON,
+};
 
 export const chapter2Lessons: { [key: string]: DetailedLessonData } = {
   't6-b8-quan-he-chia-het': LESSON_8_DATA,
@@ -13,5 +23,6 @@ export const chapter2Lessons: { [key: string]: DetailedLessonData } = {
   't6-b10-dau-hieu-chia-het-3-9': LESSON_10_DATA,
   't6-b11-so-nguyen-to': LESSON_11_DATA,
   't6-b12-uoc-chung-ucln': LESSON_12_DATA,
+  't6-b13-boi-chung-bcnn': LESSON_13_DATA,
+  't6-on-tap-chuong-2': GRADE_6_CHAPTER_2_REVIEW_LESSON,
 };
-
